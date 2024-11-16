96646cd25e3b4dc5b41315c10b7f13e5267c2e9f
Mehakpreet2004 <122347713+Mehakpreet2004@users.noreply.github.com>
2024-11-16 17:43:35 +0530

    Update Message.jsx

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -6,7 +6,7 @@ import { extractTime } from "../../utils/extractTime";
 const Message = ({ message }) => {
   const { authUser } = useAuthContext();
   const { selectedConversation } = useConversation();
-  const fromMe = message.senderId === authUser._id;
+  const fromMe = message.senderId === authUser?._id;
   const formattedTime = extractTime(message.createdAt);
   const chatClassName = fromMe ? "chat-end" : "chat-start";
   const profilePic = fromMe
